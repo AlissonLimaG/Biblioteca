2cc51c98a304d57f560bf01a6d0da763672f283d
Alisson Gabriel Pereira de Lima <alissongabriel010907@gmail.com>
2024-07-17 15:00:55 -0300

    main pt 1

---
+++ index/index.js
@@ -0,0 +1,9 @@
+export const caminho = ['background.jpg', 'background2.jpg', 'background3.jpg', 'background4.jpg', 'background5.jpg'];
+export const frases = [
+"Programar é pensar, não digitar.",
+"A programação é uma arte, e você deve sempre se esforçar para criar arte.",
+"O código é como o humor. Quando você tem que explicar, não é tão bom.",
+"Primeiro, resolva o problema. Então, escreva o código.",
+"A melhor maneira de prever o futuro é implementá-lo."
+]
+export const autores = ['Harlan Mills','Ron Jeffries','Cory House','John Johnson','David Heinemeier Hansson']
